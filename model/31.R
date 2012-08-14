effa41d5701ca158447f0e42f1e3cbc21ca087de
luoq <luoq08@gmail.com>
2012-08-14 15:37:11 +0800

    fix erroneous cv process in feature selection

--- model/31.R
+++ model/31.R
@@ -1,9 +1,10 @@
-comment="NB.Multinomial on 1-3-gram,feature selection by information gain"
+comment="NB.Multinomial on 1-5-gram,IG,laplace=1e-3"
 require(Metrics)
 source('general/util.R')
 used_feature <- c(simple=FALSE,dtm=TRUE,corpus=FALSE)
-dtm_features_ctrl <- list(mingram=1,maxgram=3,local_weight="tf",term_weight=NULL)
-train.NB.Multinomial <- function(X,y,laplace=1e-4){
+dtm_features_ctrl <- list(mingram=1,maxgram=5,local_weight="tf",term_weight=NULL)
+LAPLACE <- 1e-3
+train.NB.Multinomial <- function(X,y,laplace=LAPLACE){
   y <- as.factor(y)
   if(is.vector(X))
     X <- matrix(X,ncol=1)
@@ -20,7 +21,7 @@ predict.NB.Multinomial <- function(model,X){
   L <- L+outer(rep(1,nrow(X)),model$logprior)
   model$levels[apply(L,1,which.max)]
 }
-train.and.predict <- function(X,y,X2,ord,ks,laplace=1e-4){
+train.and.predict <- function(X,y,X2,ord,ks,laplace=LAPLACE){
   y <- as.factor(y)
   levels=as.numeric(levels(y))
   if(is.vector(X))
@@ -45,26 +46,27 @@ train.and.predict <- function(X,y,X2,ord,ks,laplace=1e-4){
   })
 }
 train.model <- function(X,y){
-  w <- informationGainMultinomial(y,X,laplace=1e-4)
-  ord <- order(w,decreasing=TRUE)
-  ks <- square.split(length(w),30)
-  
   K <- 5
   n <- length(y)
   all.folds <- split(1:n,rep(1:K,length=n))
   kappa <- sapply(1:K,function(k){
     omit <- all.folds[[k]]
-    nb <- train.NB.Multinomial(X[-omit,,drop=FALSE],y[-omit])
-    pred <- predict.NB.Multinomial(nb,X[omit,,drop=FALSE])
+    w <- informationGainMultinomial(y[-omit],X[-omit,,drop=FALSE],laplace=LAPLACE)
+    ord <- order(w,decreasing=TRUE)
+    ks <- weight.split(sort(w,decreasing=TRUE),100)
     pred <- train.and.predict(X[-omit,,drop=FALSE],y[-omit],X[omit,,drop=FALSE],ord,ks)
     kappa <- apply(pred,2,function(pred)
                    ScoreQuadraticWeightedKappa(pred,y[omit]))
   })
   mean.kappa <- apply(kappa,1,MeanQuadraticWeightedKappa)
   i <- which.max(mean.kappa)
-  subset <- ord[1:ks[i]]
   kappa <- c(kappa[i,],mean.kappa[i])
   names(kappa) <- c(sapply(as.character(1:K),function(x) paste("fold",x,sep="")),"mean")
+  
+  w <- informationGainMultinomial(y,X,laplace=LAPLACE)
+  ord <- order(w,decreasing=TRUE)
+  ks <- weight.split(sort(w,decreasing=TRUE),100)
+  subset <- ord[1:ks[i]]
   
   nb <- train.NB.Multinomial(X[,subset,drop=FALSE],y)
   model <- list(subset=subset,nb=nb)
