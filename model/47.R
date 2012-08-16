dcf3cba18e93dbe1583b92a6c185caa72320e76b
luoq <luoq08@gmail.com>
2012-08-16 10:59:34 +0800

    some trys

---
+++ model/47.R
@@ -0,0 +1,40 @@
+comment="glmnet on sf,calibrated with NB,no standardize"
+require(Metrics)
+require(glmnet)
+source('general/util.R')
+source('model/auxiliary.R')
+used_feature <- c(simple=TRUE,dtm=FALSE,corpus=FALSE)
+dtm_features_ctrl <- list(mingram=1,maxgram=3,local_weight="bintf",term_weight=NULL)
+apply.model <- function(model,sf){
+  X <- as.matrix(sf)
+  pred <- predict(model$fit,X,s=model$s)
+  pred <- apply.multi.NB.normal(model$nb,pred)
+}
+train.model <- function(sf,y){
+  X <- as.matrix(sf)
+  yrange <- range(y)
+  K <- 5
+  n <- length(y)
+  all.folds <- split(1:n,rep(1:K,length=n))
+
+  fit <- glmnet(X,y,alpha=0.8,nlambda=100,standardize=FALSE,family="gaussian")
+  lambda <- fit$lambda
+  kappa <- sapply(1:K,function(k){
+    omit <- all.folds[[k]]
+    fit <- glmnet(X[-omit,,drop=FALSE],y[-omit],lambda=lambda,alpha=0.8,,standardize=FALSE,family="gaussian")
+    nb <- train.multi.NB.normal(predict(fit,X[-omit,,drop=FALSE]),y[-omit])
+    pred <- predict(fit,X[omit,,drop=FALSE])
+    pred <- apply.multi.NB.normal(nb,pred)
+    kappa <- apply(pred,2,function(pred)
+                   ScoreQuadraticWeightedKappa(pred,y[omit]))
+  })
+  mean.kappa <- apply(kappa,1,MeanQuadraticWeightedKappa)
+  i <- which.max(mean.kappa)
+  s <- lambda[i]
+  kappa <- c(kappa[i,],mean.kappa[i])
+  names(kappa) <- c(sapply(as.character(1:K),function(x) paste("fold",x,sep="")),"mean")
+
+  nb <- train.multi.NB.normal(predict(fit,X,s=s),y)
+  model <- list(fit=fit,s=s,nb=nb)
+  return(list(model=model,kappa=kappa))
+}
