83e9a1464aa2ad0b44b6b9667d5081e3841b8f9f
luoq <luoq08@gmail.com>
2012-08-28 16:04:58 +0800

    optimize by classify precision

--- model/14.R
+++ model/14.R
@@ -1,10 +1,10 @@
-description="AdaBoost on Glmnet.with.NB"
+description="AdaBoost on Glmnet.with.NB optimize precicion"
 used.feature <- c(simple=FALSE,dtm=TRUE,corpus=FALSE)
 dtm.feature.ctrl <- list(mingram=1,maxgram=3,local_weight="bintf",term_weight=NULL)
 L <- function(X,y){
   model <- AdaBoost(X,y,function(...)
-                    CV.Glmnet.with.NB(...,cv.ctrl=list(K=5))$model,
+                    CV.Glmnet.with.NB(...,cv.ctrl=list(K=5,main.measure=2))$model,
                     5)
 }
 train.model <- function(X,y)
-  CV(X,y,train.f=L)
+  CV(X,y,K=5,train.f=L)
