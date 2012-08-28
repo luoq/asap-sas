8e4cef9ce3fc60359f34dad7178331eddc4eca81
luoq <luoq08@gmail.com>
2012-08-28 17:03:50 +0800

    AdaBoost estimate error on hold out set

--- AdaBoost.R
+++ AdaBoost.R
@@ -18,7 +18,36 @@ AdaBoost <- function(X,y,learner,T){
     hs <- c(hs,list(h))
     betas <- c(betas,beta)
   }
-  model <- list(hs=hs,minus.log.betas=-log(betas))
+  model <- list(hs=hs,minus.log.betas=-log(betas),Ds=Ds)
+  class(model) <- c("AdaBoost.model","list")
+  model
+}
+AdaBoost.V1 <- function(X,y,learner,T,self.cv=TRUE,cv.ctrl=NULL){
+  n <- length(y)
+  D <- rep(1,n)/n
+  hs <- NULL
+  betas <- NULL
+  Ds <- D
+  for(t in 1:T){
+    res <- 
+      if(self.cv)
+        learner(X,y,weights=D)
+      else
+        do.call(CV,c(list(X,y,weights=D,train.f=learner),cv.ctrl))
+    h <- res$model
+    pred <- as.vector(aggregate.CV.result(res,what="class"))
+    mask <- pred==y
+    epsilon <- sum(D[!mask])
+    if(epsilon > 1/2)
+      break
+    beta <- epsilon/(1-epsilon)
+    D[mask] <- D[mask]*beta
+    D <- D/sum(D)
+    Ds <- cbind(Ds,D)
+    hs <- c(hs,list(h))
+    betas <- c(betas,beta)
+  }
+  model <- list(hs=hs,minus.log.betas=-log(betas),Ds=Ds)
   class(model) <- c("AdaBoost.model","list")
   model
 }
