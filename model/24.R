d8bfd32d4092d764d509ad647b64cce987b7eb91
luoq <luoq08@gmail.com>
2012-08-29 08:31:29 +0800

    missing model file

---
+++ model/24.R
@@ -0,0 +1,8 @@
+description="AdaBoost.V1 on Glmnet.with.NB optimize precicion"
+used.feature <- c(simple=FALSE,dtm=TRUE,corpus=FALSE)
+dtm.feature.ctrl <- list(mingram=1,maxgram=3,local_weight="bintf",term_weight=NULL)
+L <- function(X,y){
+  model <- AdaBoost.V1(X,y,function(...) CV.Glmnet.with.NB(...,cv.ctrl=list(K=5,main.measure=2)),5,square.weight=TRUE)
+}
+train.model <- function(X,y)
+  CV(X,y,K=5,train.f=L)
