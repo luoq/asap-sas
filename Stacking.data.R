559f6714b4d84fcc8ea6755eb6ffbf5211196a31
luoq <luoq08@gmail.com>
2012-08-29 12:55:24 +0800

    functions to try meta-classifier

--- Stacking.data.R
+++ Stacking.data.R
@@ -1,9 +1,9 @@
 get.Stacking.data <- function(X,y,learners.info=NULL){
-  temp <- lapply(learners.info,function(l){
-    res <- l(X,y)
+  temp <- lapply(learners.info,function(x){
+    res <- x$learner(X,y)
     class <- aggregate.CV.result(res,what="class")
     prob <- aggregate.CV.result(res,what="prob")
-    if(!is.null(l$remove.one) || l$remove.one)
+    if(is.null(x$remove.one) || x$remove.one)
       prob <- prob[,1:(ncol(prob)-1)]
     list(f=res$model,class=class,prob=prob)
   })
@@ -33,4 +33,45 @@ predict.Stacking.data.model <- function(model,X){
   prob <- lapply(temp,function(x) x$prob)
   prob <- do.call(cbind,prob)
   list(prob=prob,class=class)
+}
+get.CV.Stacking.data <- function(X,y,f,K=5,split="random"){
+  n <- length(y)
+  all.folds <- if(split=="random")
+    all.folds <-cv.kfold.random(n,K)
+  else if(split=="sequential")
+    cv.kfold.sequential(n,K)
+  else if(split=="stratified")
+    cv.kfold.stratified.random(y,K)
+  else
+    stop("no such split method")
+  ret <- list(all.folds=all.folds)
+
+  res <- lapply(1:K,function(k){
+    omit <- all.folds[[k]]
+    X1 <- X[-omit,,drop=FALSE]
+    y1 <- y[-omit]
+    X2 <- X[omit,,drop=FALSE]
+    y2 <- y[omit]
+
+    res1 <- f(X1,y1)
+    class1 <- res1$class
+    prob1 <- res1$prob
+    model <- res1$model
+    res2 <- predict(model,X2)
+    class2 <- res2$class
+    prob2 <- res2$prob
+    list(model=model,prob1=prob1,class1=class1,prob2=prob2,class2=class2,y1=y1,y2=y2)
+  })
+}
+assess.meta.classifer <- function(train.f,result){
+  kappas <- sapply(result,function(x){
+    with(x,{
+      f <- train.f(prob1,y1)
+      if(
+      pred2 <- predict(f,prob2)$class
+      ScoreQuadraticWeightedKappa(pred2,y2)
+    })
+  })
+  mean.kappa <- MeanQuadraticWeightedKappa(kappas)
+  return(list(kappa=kappas,mean.kappa=kappa)))
 }
--- model/49.R
+++ model/49.R
@@ -6,14 +6,14 @@ D <- function(X,y){
          list(learner=prebinarizer(
                 function(X,y)
                 CV.Glmnet.with.NB.2(X,y,cv.ctrl=list(K=K,main.measure=main.measure)))),
-         list(learner=function(X,y)
-              CV.NB.Multinomial.Best.K(X,y,weight.fun="informationGain2",
-                                       ks=4+square.split(ncol(X)-4,100),
-                                       cv.ctrl=list(K=K,main.measure=main.measure))),
-         list(learner=prebinarizer(
-                function(X,y)
-                CV.Glmnet(X,y,glmnet.ctrl=list(alpha=0.8,standardize=FALSE,family="multinomial"),
-                          cv.ctrl=list(K=K,main.measure=main.measure)))),
+#         list(learner=function(X,y)
+#             CV.NB.Multinomial.Best.K(X,y,weight.fun="informationGain2",
+#                                       ks=4+square.split(ncol(X)-4,100),
+#                                       cv.ctrl=list(K=K,main.measure=main.measure))),
+#         list(learner=prebinarizer(
+#                function(X,y)
+#              CV.Glmnet(X,y,glmnet.ctrl=list(alpha=0.8,standardize=FALSE,family="multinomial"),
+#                         cv.ctrl=list(K=K,main.measure=main.measure)))),
          list(learner=prebinarizer(
                 function(X,y)
                 CV(X,y,train.f=function(X,y) train.LiblineaR.model(X,y,type=5,cost=0.1),
@@ -21,3 +21,5 @@ D <- function(X,y){
               remove.one=FALSE))
   get.Stacking.data(X,y,learners.info=learners.info)
 }
+CV.D <- function(X,y)
+  get.CV.Stacking.data(X,y,D)
